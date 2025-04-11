40be98be879367c7ae14a6bd9b856958a07fd74d
LAB  INDUSTRI - 32 <nurulsandrinamarhas@gmail.com>
2025-04-11 13:37:30 +0700

    tugas 2

--- view/barang/index.php
+++ view/barang/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -27,7 +28,7 @@
 </nav>
 
 <div class="container"> 
-  <h1>Data Barang Sandrina</h1>
+  <h1><i class="fa-solid fa-user"></i>Data Barang Sandrina</h1>
   <a href="view_tambah.php" class="btn btn-primary"> </i>>Tambah Data Barang Baru</a>
 <table class="table">
   <thead>
